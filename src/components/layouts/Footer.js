2464d07b0a7d113463067943d50b72442caa8809
rjRenolo <rjrenolo14@gmail.com>
2020-01-05 15:23:49 +0800

    Added Categories on Tabs

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Paper, Tabs, Tab } from '@material-ui/core';
 
-const Footer = () => {
+const Footer = ({ muscles }) => {
   return (
     <Paper>
-      <Tabs value={1} indicatorColor="primary" textColor="primary" centered>
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+      <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
+        <Tab label="All" />
+        {muscles.map(categ => (
+          <Tab label={categ} />
+        ))}
       </Tabs>
     </Paper>
   );
