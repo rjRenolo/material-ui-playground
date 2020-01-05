422f74fca6a6920f646c190807448441cc2ef8e2
rjRenolo <rjrenolo14@gmail.com>
2020-01-05 15:37:03 +0800

    categorizing data

--- src/App.js
+++ src/App.js
@@ -9,12 +9,29 @@ class App extends Component {
   state = {
     exercises
   };
+
+  getExercisesByCategory = () => {
+    return Object.entries(
+      this.state.exercises.reduce((accumulator, exercise) => {
+        // turned into array
+        const { muscles } = exercise;
+
+        // dang gonna study this categorizing method... look it up in store.js for the data pertaining this shiz...
+        accumulator[muscles] = accumulator[muscles]
+          ? [...accumulator[muscles], exercise]
+          : [exercise];
+        return accumulator;
+      }, {})
+    );
+  };
+
   render() {
+    const data = this.getExercisesByCategory();
     return (
       <Fragment>
         <Header />
 
-        <Exercises />
+        <Exercises exercises={data} />
 
         <Footer muscles={muscles} />
       </Fragment>
