be07855cc4fe0c679290521541860e6f65910cab
rjRenolo <rjrenolo14@gmail.com>
2020-01-05 15:08:46 +0800

    todo

--- src/components/exercises/LeftPane.js
+++ src/components/exercises/LeftPane.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Paper } from '@material-ui/core';
 const LeftPane = ({ styles }) => {
+  // list of exercises
   return <Paper style={styles.Paper}>Left Panel</Paper>;
 };
 
--- src/components/exercises/RightPane.js
+++ src/components/exercises/RightPane.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Paper } from '@material-ui/core';
 
 const RightPane = ({ styles }) => {
+  // details
   return <Paper style={styles.Paper}>Right Panel</Paper>;
 };
 
